e195ffae2d82154fa28b0f42668730e1d56ca26a
Andrew Jenkins <stumptownandy@gmail.com>
2016-03-24 15:17:10 -0600

    Update

--- service.js
+++ service.js
@@ -1,32 +1,8 @@
-var app = angular.module("github-api");
-app.service('githubService', function($http) {
+angular.module()
+
+.factory('myService', function () {
+  var service = {};
 
 
-var id = "myCLientID";
-var sec = "mySecretKeyThing";
-var param = "?=3257a93c96b37cf80ae9" + id + "&=45becb1dda65f5af435cb81048b8791bd02440f9" + sec;
-var user ="username";
-this.getUser = function(username) {
-    
-    return $http({ 
-        method: 'GET', 
-        url: 'https://api.github.com/users/'+ username
-       });
-    }
+  return service;
 });
-
-
-
-var express = require('express')
-var app = express ();
-
-app.get('/'), function(request, response){
-    response.send('you should go here:'
-        +' <a href="/api/starwars"> Click here to see star wars</a>'         
-})
-    app.post('/api/starwars', function(req,res){
-        LastPostToStarWars = req.body.provoMessage
-        res.send ("Post Successful");
-    })
-    
-    app.listen('3000')
